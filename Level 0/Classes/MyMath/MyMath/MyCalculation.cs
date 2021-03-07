7dbc7f7479303cdad7611c481957826cfe0d21ee
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-03-07 11:00:57 +0200

    added a simple MyMath project which uses an auto property

---
+++ Level 0/Classes/MyMath/MyMath/MyCalculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyMath
+{
+  public class MyCalculation
+  {
+    public int Total { get; set; }
+
+    public MyCalculation(int total)
+    {
+      Total = total;
+    }
+
+    public void AddSumToTotal(int input1, int input2)
+    {
+      Total += input1 + input2;
+    }
+
+    public void AddDifferenceToTotal(int input1, int input2)
+    {
+      Total += input1 - input2;
+    }
+
+    public void AddMultiplicationToTotal(int input1, int input2)
+    {
+      Total += input1 * input2;
+    }
+
+    public void AddDivisionToTotal(int input1, int input2)
+    {
+      Total += input1 / input2;
+    }
+  }
+}
