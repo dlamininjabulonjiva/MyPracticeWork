80b5c9015a6061c9fb2128bae7dd00decdf64ed1
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-03-07 09:51:22 +0200

    added a ne MyBankAccount project

---
+++ Level 0/MyBankAccount/MyBankAccount/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBankAccount
+{
+  public class BankAccount
+  {
+    private string _customerName;
+    private double _balance;
+
+    public BankAccount(string customerName, double balance)
+    {
+      _customerName = customerName;
+      _balance = balance;
+    }
+
+    public double Balance
+    {
+      get => _balance;
+      set => _balance = value;
+    }
+
+    public void Debit(double amount)
+    {
+      if (_balance == 0)
+      {
+        throw new InvalidOperationException("Your Balance is 0");
+      }
+
+      if (amount > _balance)
+      {
+        throw new InvalidOperationException("Amount is greater than your balance");
+      }
+
+      _balance -= amount;
+    }
+
+    public void Credit(double amount)
+    {
+      if (amount < 0)
+      {
+        throw new InvalidOperationException("Amount provided is less than 0");
+      }
+
+      _balance += amount;
+    }
+  }
+}
